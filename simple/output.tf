0fe54468d6dd91984a5798e895449612cadd95ee
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2023-11-09 15:25:36 +0530

    Update output.tf

--- simple/output.tf
+++ simple/output.tf
@@ -2,3 +2,17 @@
 output "vpc_cidr_block" {
   value = aws_vpc.lms-vpc.cidr_block
 }
+output "public-ip" {
+  description = "Public IP"
+  value       = aws_instance.test-ec2.*.public_ip
+}
+
+output "publicdns" {
+  description = "URL EC2 Instance"
+  value       = aws_instance.test-ec2.*.public_dns
+}
+
+output "instance_username" {
+  description = "Username of the EC2 instance"
+  value       = aws_instance.test-ec2.key_name == "ubuntu" ? "ubuntu" : "ec2-user"
+}
