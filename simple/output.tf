ced2b98b6281d936fece5057cbd4352410ba0492
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2023-11-09 12:52:42 +0530

    Update output.tf

--- simple/output.tf
+++ simple/output.tf
@@ -1,11 +1,3 @@
-provider "aws" {
-  region = "ap-south-1" # Replace with your desired AWS region
-}
-
-resource "aws_vpc" "lms-vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
 output "vpc_id" {
   value = aws_vpc.lms-vpc.id
 }
