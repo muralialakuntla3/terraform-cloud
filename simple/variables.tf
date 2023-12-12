f7b184bf8ca7d5941912c9c016d54ca9834dd6a0
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2023-12-12 11:22:58 +0530

    Update variables.tf

--- simple/variables.tf
+++ simple/variables.tf
@@ -3,3 +3,8 @@ variable "aws_region" {
   type        = string
   default     = "ap-south-1"
 }
+variable "aws_access" {
+  description = "Region of AWS"
+  type        = string
+  default     = "KJEBDKJwfnlsnf"
+}
